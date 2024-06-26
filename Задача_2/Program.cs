3c731b9a45ce68cbec438d8e8d59ac424d40e758
Сидоров Артем <sidoroffartemii@gmail.com>
2024-06-26 22:50:16 +0300

    Добавьте файлы проекта.

---
+++ Задача_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Я пошел {{гулять}} на работу";
+            int first;
+            int last;
+            int length;
+            first = str.IndexOf("{");
+            last = str.IndexOf("}");
+            length = last-first;
+            str = str.Remove(first, length+2);
+            Console.WriteLine(str);
+            Console.ReadLine();
+            
+            
+            
+            
+        }
+    }
+}
---
+++ Задача_2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "");
+            str = str.ToLower();
+            string str2 = "";
+            foreach (char c in str)
+            {
+
+                str2 = c + str2;
+
+            }
+            str2 = str2.Replace(" ", "");
+            str2 = str2.ToLower();
+            if (str2 == str) Console.WriteLine("Строка является палиндромом");
+            else
+            Console.WriteLine("Строка не является палиндромом");
+            Console.ReadLine();
+        }
+    }
+}
